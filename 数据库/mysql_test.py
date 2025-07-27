2f551a7b09ec3d4e8b6eb6ee073fc856b1360a12
johnx <xyzdxb@126.com>
2025-07-27 20:02:49 +0800

    update

--- 数据库/mysql_test.py
+++ 数据库/mysql_test.py
@@ -9,15 +9,29 @@ import pymysql
 #     password='mysql110',
 # )
 
-conn = pymysql.connect(host='10.211.55.4', port=3306, user='root', password='mysql110', db='learning')
+conn = pymysql.connect(host='10.211.55.4', port=3306, user='root', passwd='mysql110', db='learning', autocommit=True, charset='utf8')
 
 print(conn.get_server_info())
 
+cursor = conn.cursor()
+cursor.execute('select * from student')
+
+res = cursor.fetchall()
+
+print(res)
+
+
+
+
+cursor.execute("insert into student values('10002', '李四', 20, '女', '河南省南阳市', '二年级')")
+
+
+
+# conn.commit()   # 数据库连接时设置自动提交则不需要commit
+
+
+
+
 conn.close()
 
-# import urllib.request
-#
-# conn = urllib.request.urlopen("mysql://root:mysql110@10.211.55.4:3306?allowPublicKeyRetrieval=true&useSSL=true&verifyServerCertificate=true")
-#
-# print(conn.read())
-# conn.close()
+
